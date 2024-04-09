44cb2200fa1bf2a27ff4303a5319b4e2195a6a97
Annsrutto <annsrutto567@gmail.com>
2024-04-09 03:32:50 +0300

    Select some Amenities to be comfortable!

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,27 +1,20 @@
-$(document).ready(function() {
-    // Variable to store Amenity IDs
-    const checkedAmenities = {};
+$(document).ready(function () {
+  const checkedAmenities = {};
 
-    // Function to update the h4 tag with checked amenities
-    const updateAmenitiesList = () => {
-        const checkedAmenitiesList = Object.values(checkedAmenities).join(', ');
-        $('.popover .amenities-list').text(checkedAmenitiesList);
-    };
+  const updateAmenitiesList = () => {
+    const checkedAmenitiesList = Object.values(checkedAmenities).join(', ');
+    $('.popover .amenities-list').text(checkedAmenitiesList);
+  };
 
-    // Listen for changes on input checkbox tags
-    $('input[type="checkbox"]').change(function() {
-        const amenityId = $(this).data('id');
-        const amenityName = $(this).data('name');
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
 
-        if ($(this).is(':checked')) {
-            // Add Amenity ID to the variable
-            checkedAmenities[amenityId] = amenityName;
-        } else {
-            // Remove Amenity ID from the variable
-            delete checkedAmenities[amenityId];
-        }
-
-        // Update the h4 tag with checked amenities
-        updateAmenitiesList();
-    });
+    if ($(this).is(':checked')) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+    updateAmenitiesList();
+  });
 });
